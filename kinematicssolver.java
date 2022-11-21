a65a73beb28b635f351c2af7fefc9df498005f41
IvanZh19 <ivanzheng930@gmail.com>
2022-11-21 21:48:03 +0000

    doesn't work yet

---
+++ kinematicssolver.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class kinematicssolver{
+    public static void main(String[] args){
+
+Scanner s = new Scanner(System.in);
+System.out.println("BRUH WHAT'S YOUR QUESTION");
+System.out.println("Enter a value for gravity:");
+double a = s.nextDouble();
+System.out.println("Enter a value for initial height:");
+double h = s.nextDouble();
+System.out.println("Enter a value for velocity, and then an initial angle:");
+double v = s.nextDouble();
+double th = s.nextDouble();
+System.out.println("Enter a value for range:");
+double r = s.nextDouble();
+
+
+
+
+
+
+
+
+
+    }
+}
